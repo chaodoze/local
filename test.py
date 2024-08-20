30891de9059909c20e3e8222d8ec999d2ddd42b6
chao <chao@nextsmallthings.com>
2024-08-19 21:29:23 -0700

    adding second function

--- test.py
+++ test.py
@@ -7,57 +7,83 @@ load_dotenv()
 th = Toolhouse()
 
 # The parameter must match the name of the tool in your tool definition
-@th.register_local_tool("get_current_weather")
-def get_weather_forecast(
+@th.register_local_tool("fetch_tweet")
+def get_tweet(
    # Must match the name of the parameters in your tool definition
-   latitude: float, 
-   longitude: float) -> str:
+   tweet_id: str) -> str:
    
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&forecast_days=1"
+    url = f"http://localhost:3000/api/tweet"
     
-    response = requests.get(url)
+    response = requests.post(url, json={"id": tweet_id})
     
     if response.status_code == 200:
         return response.text
     else:
         return f"Error: {response.status_code} - {response.text}"
 
+@th.register_local_tool("search_recent_tweets")
+def search_tweets(
+   query: str
+) -> str:
+    return "<tweet>this is a tweet about covid</tweet>"
+    url = f"http://localhost:3000/api/search_recent"
+    response = requests.post(url, json={"query": query})
+    if response.status_code == 200:
+        return response.text
+    else:
+        return f"Error: {response.status_code} - {response.text}"
+
 my_local_tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "get_current_weather",
-            "description": "Retrieves the current weather for the location you specify.",
-            "parameters": {
-              "type": "object",
-              "properties": {
-                  "latitude": {
-                      "type": "number",
-                      "description": "The latitude of the location.",
-                  },
-                  "longitude": {
-                      "type": "number",
-                      "description": "The longitude of the location.",
-                  },
+  {
+    "type": "function",
+    "function": {
+        "name": "fetch_tweet",
+        "description": "Fetches the tweet from the tweet id",
+        "parameters": {
+          "type": "object",
+          "properties": {
+              "tweet_id": {
+                  "type": "string",
+                  "description": "The id of the tweet.",
               },
-              "required": [
-                  "latitude",
-                  "longitude",
-              ],
-            },
+          },
+          "required": [
+              "tweet_id",
+          ],
         },
-    }
+    },
+  },
+  {
+    "type": "function",
+    "function": {
+        "name": "search_recent_tweets",
+        "description": "Search for recent tweets in the past 7 days given a query",
+        "parameters": {
+          "type": "object",
+          "properties": {
+              "query": {
+                  "type": "string",
+                  "description": "Query to search for recent tweets.",
+              },
+          },
+          "required": [
+              "query",
+          ],
+        },
+    },
+  }
 ]
 
+
 client = OpenAI()
-MODEL = "gpt-4o-mini"
+MODEL = "gpt-4o"
 # If you don't specify an API key, Toolhouse will expect you
 # specify one in the TOOLHOUSE_API_KEY env variable.
 
 messages = [
   {
     "role": "user",
-    "content": "What's the weather in Palo Alto, CA?",
+    "content": "Search for recent tweets about #covid",
   }
 ]
 
@@ -77,7 +103,7 @@ print(messages[1])
 response = client.chat.completions.create(
   model=MODEL,
   messages=messages,
-  tools=th.get_tools(),
+  tools=th.get_tools()+my_local_tools,
 )
 
 print(response.choices[0].message.content)
